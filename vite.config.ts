ab42d93414b13d3d6873d6c41f976c0b8c6bd350
Nina W <nina@Ninas-Air.lan>
2025-05-18 14:57:55 -0700

    small fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000, // Optional, use Render's PORT variable during deployment
+    port: 3000,
+    allowedHosts: [
+      'module-13-challenge-cwqz.onrender.com',
+      'localhost',
+    ],
   },
   preview: {
+    host: '0.0.0.0',
+    port: 4173,
     allowedHosts: [
-      'module-13-challenge-cwqz.onrender.com',  
+      'module-13-challenge-cwqz.onrender.com',
       'localhost',
     ],
   },
 });
-
-
-
-//envDir: './env',
